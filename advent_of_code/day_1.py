58bc49b6b37a1ce2bd3c21f23e8cf5adee86310c
chhk <chorhatarahuduketuri@gmail.com>
2023-12-09 10:35:28 +0000

    Day 1 Star 2

--- advent_of_code/day_1.py
+++ advent_of_code/day_1.py
@@ -1,4 +1,5 @@
 from settings import PUZZLE_INPUT_PATH
+import re
 
 
 def compute_star_1(puzzle_input):
@@ -27,4 +28,34 @@ def compute_star_1(puzzle_input):
     )
 
 
+def compute_star_2(puzzle_input):
+    """The lookahead assertion ?=(...) matches, but doesn't consume any of the string, so it doesn't hide overlaps"""
+    numbers = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+    return sum(
+        [
+            int(
+                (numbers[x[0]] if numbers.get(x[0]) else x[0])
+                + (numbers[x[-1]] if numbers.get(x[-1]) else x[-1])
+            )
+            for x in [
+                re.findall(
+                    r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line
+                )
+                for line in puzzle_input.strip().split()
+            ]
+        ]
+    )
+
+
 print("day 1, star 1: ", compute_star_1(open(PUZZLE_INPUT_PATH / "day1.txt").read()))
+print("day 1, star 2: ", compute_star_2(open(PUZZLE_INPUT_PATH / "day1.txt").read()))
--- tests/test_day_1.py
+++ tests/test_day_1.py
@@ -6,11 +6,27 @@ input_1_day_1 = """1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet"""
+
+input_2_day_1 = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+
 output_day_1_star_1 = 142
+output_day_1_star_2 = 281
 
 day_1_star_1_test_input = [(input_1_day_1, output_day_1_star_1)]
+day_1_star_2_test_input = [(input_2_day_1, output_day_1_star_2)]
 
 
 @pytest.mark.parametrize("puzzle_input, output", day_1_star_1_test_input)
 def test_day_1_star_1(puzzle_input, output):
     assert day_1.compute_star_1(puzzle_input) == output
+
+
+@pytest.mark.parametrize("puzzle_input, output", day_1_star_2_test_input)
+def test_day_1_star_2(puzzle_input, output):
+    assert day_1.compute_star_2(puzzle_input) == output
